984f3e64b909e0559a067c34eff3ad5b38e4222c
rsjwy <1446176202@qq.com>
2025-10-25 17:46:02 +0800

    添加comfyui

--- Assets/RSJWYFamework/Runtime/Config/AppConfigManager.cs
+++ Assets/RSJWYFamework/Runtime/Config/AppConfigManager.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO;
 using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RSJWYFamework.Runtime
 {
@@ -8,6 +13,92 @@ namespace RSJWYFamework.Runtime
     public class AppConfigManager:ModuleBase
     {
         public AppConfig AppConfig { get; private set; }
+
+        public string JsonConfigPath = Application.streamingAssetsPath;
+        public ConcurrentDictionary<string,JObject> JsonConfigDict { get; private set; } = new ConcurrentDictionary<string, JObject>();
+
+        /// <summary>
+        /// 获取配置文件
+        /// </summary>
+        /// <param name="name">配置文件名</param>
+        /// <returns>配置文件内容，为JObject类型</returns>
+        public JObject GetConfig(string name)
+        {
+            JObject _output = null;
+            if (JsonConfigDict.TryGetValue(name, out _output))
+            {
+                return _output;
+            }
+            AppLogger.Warning($"AppConfigManager GetConfig {name} is not found,try to load from {JsonConfigPath}");
+            if (LoadConfig(name))
+            {
+                if (JsonConfigDict.TryGetValue(name, out _output))
+                {
+                    return _output;
+                }
+                throw new Exception($"AppConfigManager LoadConfig {name} is not found！！path is {JsonConfigPath},这是个不可能发生的错误");
+            }
+
+            AppLogger.Error($"AppConfigManager LoadConfig {name} is failed,path is {JsonConfigPath}");
+            return null;
+        }
+        /// <summary>
+        /// 设置配置文件
+        /// </summary>
+        /// <remarks>
+        /// 该方法会覆盖原有配置文件，并写入文件
+        /// </remarks>
+        /// <param name="name">配置文件名</param>
+        /// <param name="config">配置文件内容，为JObject类型</param>
+        public void SetConfig(string name, JObject config)
+        {
+            //覆盖原有配置文件
+            JsonConfigDict.AddOrUpdate(
+                name, config, 
+                (key, oldValue) => config
+            );
+            //写入文件
+            string path = $"{JsonConfigPath}/{name}.json";
+            try
+            {
+                Utility.FileAndFolder.EnsureDirectoryExists(path);
+                File.WriteAllText(path, config.ToString());
+            }
+            catch (Exception ex)
+            {
+                AppLogger.Error($"AppConfigManager SetConfig {name} to file is failed,path is {path},error is {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// 加载配置文件
+        /// </summary>
+        /// <param name="name">配置文件名</param>
+        /// <returns>是否加载成功</returns>
+        public bool LoadConfig(string name)
+        {
+            string path = $"{JsonConfigPath}/{name}.json";
+            if (!File.Exists(path))
+            {
+                Debug.LogError($"AppConfigManager LoadConfig {name} is not found,path is {path}");
+                return false;
+            }
+            try
+            {
+                string json = File.ReadAllText(path);
+                JObject jsonObject = JObject.Parse(json); // 解析失败会抛出异常
+                JsonConfigDict[name] = jsonObject;
+                return true;
+            }
+            catch (Exception ex) // 捕获解析异常（如格式错误、非对象结构等）
+            {
+                Debug.LogError($"解析JSON失败，路径：{path}，错误：{ex.Message}");
+                return false;
+            }
+        }
+        
+        
+        
         public override void Initialize()
         {
             AppConfig = ModuleManager.GetModule<DataManager>().GetFirstDataSB<AppConfig>();
---
+++ Assets/RSJWYFamework/Runtime/Other/ComfyUI/ComfyUIManager.cs
@@ -0,0 +1,77 @@
+using System;
+using Cysharp.Threading.Tasks;
+using TouchSocket.Core;
+using TouchSocket.Http.WebSockets;
+using TouchSocket.Sockets;
+using Newtonsoft.Json;
+
+namespace RSJWYFamework.Runtime
+{
+    [Module()]
+    public class ComfyUIManager:ModuleBase
+    {
+        public string ClientID;
+        public string ComfyUIRemote="127.0.0.1:8188";
+        private WebSocketClient ComfyUIWSClient;
+        public bool UseWss = false;
+        public override void Initialize()
+        {
+            UpdateGuid();
+            ConnectComfyUI().Forget();
+        }
+
+        public override void Shutdown()
+        {
+            ComfyUIWSClient?.Dispose();
+        }
+        /// <summary>
+        /// 连接ComfyUI websocket
+        /// </summary>
+        /// <returns>是否连接成功</returns>
+        public async UniTask<bool> ConnectComfyUI()
+        {
+            ComfyUIWSClient?.Dispose();
+            try
+            {
+                var _wsConfig = new TouchSocketConfig()
+                    .ConfigureContainer(a =>
+                    {
+                        a.AddLogger(TouchSocketContainerUnityDebugLogger.Default);
+                    })
+                    .ConfigurePlugins(a =>
+                    {
+                        a.UseWebSocketReconnection();
+                        //使用健康插件进行绝对存活检测，默认10秒检测一次。
+                        a.UseWebSocketHeartbeat();
+                    })
+                    .SetRemoteIPHost($"{(UseWss?"wss":"ws")}://{ComfyUIRemote}/ws?client_id={ClientID}");
+                ComfyUIWSClient = new WebSocketClient();
+                await ComfyUIWSClient.SetupAsync(_wsConfig);
+                await ComfyUIWSClient.ConnectAsync();
+                AppLogger.Log("ComfyUI websocket connected");
+                return true;
+            }
+            catch (Exception e)
+            {
+                AppLogger.Error($"ComfyUI websocket connect failed,error is {e.Message}");
+                return false;
+            }
+            
+        }
+        public void UpdateGuid(string clientID="")
+        {
+            if (string.IsNullOrEmpty(clientID))
+            {
+                ClientID = Guid.NewGuid().ToString();
+            }
+            else
+            {
+                ClientID = clientID;
+            }
+        }
+        
+    }
+    
+    
+    
+}
